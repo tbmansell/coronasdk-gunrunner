6b36d9090e7a83ea37dcaf43459a44189d18a791
enj <ian@topgrade.org.uk>
2017-11-01 22:53:51 +0000

    turrets

--- elements/player.lua
+++ elements/player.lua
@@ -10,8 +10,8 @@ local Player = {
     intHeight         = 25,
     intWidth          = 25,
     intMaxHealth      = 20,
-    verticalSpeed     = 5,
-    strafeSpeed       = 5,
+    verticalSpeed     = 4,
+    strafeSpeed       = 4,
     powerupDuration   = 10000,
 
     mode              = PlayerMode.ready,

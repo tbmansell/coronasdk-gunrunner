ff8dfc4ac129cd0a47f515bde854349b6ad6e584
toby mansell <tobymansell@tobys-iMac.local>
2017-11-16 22:45:54 +0000

    laserbolt

--- constants/globals.lua
+++ constants/globals.lua
@@ -58,8 +58,8 @@ Weapons = {
     laserGun          = {name="laserGun",    damage=2, ammoType="laser",     speed=600, rof=400,  ammo=15, reload=1300, collect=50, ricochet=2},
     chainGun          = {name="chainGun",    damage=1, ammoType="chainShot", speed=700, rof=150,  ammo=50, reload=2000, collect=50, burst=3},
     chainGunTurret    = {name="chainGun",    damage=1, ammoType="chainShot", speed=700, rof=150,  ammo=50, reload=2000, collect=50, burst=3},
-    laserCannon       = {name="laserCannon", damage=4, ammoType="laserBolt", speed=600, rof=1200, ammo=20, reload=2000, collect=50, shootThrough=true},
-    laserCannonTurret = {name="laserCannon", damage=4, ammoType="laserBolt", speed=600, rof=1200, ammo=20, reload=2000, collect=50, shootThrough=true},
+    laserCannon       = {name="laserCannon", damage=4, ammoType="laserBolt", speed=600, rof=900, ammo=20, reload=2000, collect=50, shootThrough=true},
+    laserCannonTurret = {name="laserCannon", damage=4, ammoType="laserBolt", speed=600, rof=900, ammo=20, reload=2000, collect=50, shootThrough=true},
 }
 
 -- Load in weapon stats for spine, sound and particles:

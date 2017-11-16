6ecd292ccc942ec355346559de9e092abec52a0c
enj <ian@topgrade.org.uk>
2017-11-16 22:36:54 +0000

    laserBolt

--- constants/globals.lua
+++ constants/globals.lua
@@ -56,8 +56,8 @@ Weapons = {
     shotgun           = {name="shotgun",           damage=1, ammoType="shell",     speed=650, rof=750,  ammo=10, reload=1200, collect=25},
     launcher          = {name="launcher",          damage=5, ammoType="rocket",    speed=550, rof=1200, ammo=4,  reload=1500, collect=50, area=75},
     laserGun          = {name="laserGun",          damage=2, ammoType="laser",     speed=600, rof=400,  ammo=15, reload=1300, collect=50, ricochet=2},
-    chainGun          = {name="chainGun",          damage=1, ammoType="bullet",    speed=700, rof=150,  ammo=50, reload=2000, collect=50, burst=3},
-    chainGunTurret    = {name="chainGun",          damage=1, ammoType="bullet",    speed=700, rof=150,  ammo=50, reload=2000, collect=50, burst=3},
+    chainGun          = {name="chainGun",          damage=1, ammoType="chainShot",    speed=700, rof=150,  ammo=50, reload=2000, collect=50, burst=3},
+    chainGunTurret    = {name="chainGun",          damage=1, ammoType="chainShot",    speed=700, rof=150,  ammo=50, reload=2000, collect=50, burst=3},
     laserCannon       = {name="laserCannon",       damage=4, ammoType="laserBolt", speed=600, rof=400,  ammo=20, reload=2000, collect=50},
     laserCannonTurret = {name="laserCannonTurret", damage=4, ammoType="laserBolt", speed=600, rof=400,  ammo=20, reload=2000, collect=50},
 }

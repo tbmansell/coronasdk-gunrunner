13a60f855d45a63b4348319712d5b2961a7bc1b4
enj <ian@topgrade.org.uk>
2017-11-17 01:19:31 +0000

    emeny death + edits

--- scenes/game.lua
+++ scenes/game.lua
@@ -140,7 +140,7 @@ end
 
 function scene:loadPlayer()
     player = level:createPlayer({xpos=11.5, ypos=-5.5}, hud)
-    player:setWeapon(Weapons.laserCannon)
+    player:setWeapon(Weapons.rifle)
     
     -- Create Game Over callback
     player.failedCallback = function()
